e25c873a1f5f5aa6053e64cf7711066c7327dd39
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-10-30 23:36:25 +0530

    added pre-installation

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,22 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, response, Response } from "express";
 import userRouter from "./routes/users";
 import cookieParser from "cookie-parser"
+import path from "path";
+import { request } from "http";
 
+const __dirname: string = path.resolve();
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json())
 
 app.use('/api/users', userRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (request, response)=>{
+  response.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 const PORT = 3000;
 
