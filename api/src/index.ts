a65f31f243688423a91418861f009350b550e76c
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-10-31 11:36:45 +0530

    updated files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,7 @@
-import express, { NextFunction, Request, response, Response } from "express";
+import express from "express";
 import userRouter from "./routes/users";
 import cookieParser from "cookie-parser"
 import path from "path";
-import { request } from "http";
 
 const __dirname: string = path.resolve();
 const app = express();
@@ -24,3 +23,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+export default app;
