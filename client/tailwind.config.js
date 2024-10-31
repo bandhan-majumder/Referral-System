817bfce42909a6a25438194c3b0bc0c7d658d876
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-10-31 11:21:59 +0530

    updated tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate";
 export default {
     darkMode: ["class"],
     content: [
@@ -15,6 +16,6 @@ export default {
   		colors: {}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 }
 
