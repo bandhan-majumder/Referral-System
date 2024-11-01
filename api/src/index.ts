77f5508a504399bc739095203fa36f1eda5e8309
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-11-01 20:57:16 +0530

    updated files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,13 @@
 import express from "express";
 import userRouter from "./routes/users";
 import cookieParser from "cookie-parser"
-import path from "path";
 
-const __dirname: string = path.resolve();
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json())
 
 app.use('/api/users', userRouter);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (request, response)=>{
-  response.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
 
 const PORT = process.env.PORT || 3000;
 
