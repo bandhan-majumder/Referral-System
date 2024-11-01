7d2dfc2047aaeaaa764ce97a269df1c5e4e7611a
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-11-01 23:49:33 +0530

    updated package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://referral-system-hers.onrender.com',
+        changeOrigin: true,
         secure: false,
       },
     },
