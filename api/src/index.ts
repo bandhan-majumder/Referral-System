7e66c05a9fe0f84f846a116b007deb32d7bd3cfb
bandhan-majumder <bandhanmajumder16@gmail.com>
2024-11-02 13:17:51 +0530

    added cors

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,13 @@
 import express, { response } from "express";
 import userRouter from "./routes/users";
 import cookieParser from "cookie-parser"
+import cors from 'cors';
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/users', userRouter);
 
